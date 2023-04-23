38afc09c180f045d509d6d8bcd539687d10fe849
Alejandro González Reyes <alexglzrys@gmail.com>
2023-04-23 12:06:04 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## TS Amazonas
+
+Aplicación Web del tipo comercio electrónico desarrollada con el conjunto de herramientas MERN y TypeScript
---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>TS Amazonas</h1>
+      </header>
+      <main></main>
+      <footer>Todos los derechos reservados</footer>
+    </>
+  )
+}
+
+export default App
